9f8c27e56982c018ae1e0d2c474fea5f6713773b
Laura Aupert <aupert.laura@gmail.com>
2021-08-28 16:27:34 -0400

    With array sorting and animation working properly

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './SortingVisualizer.css'
-import mergeSort from '../algorithms/mergeSort'
+import * as sortingAlgorithms from '../algorithms/sortingAlgorithms'
 
 export default class SortingVisualizer extends React.Component {
     constructor(props) {
@@ -24,25 +24,70 @@ export default class SortingVisualizer extends React.Component {
     }
 
     mergeSort() {
-        const testSort = this.state.array.slice().sort((a, b) => a - b);
-        const sortedArray = mergeSort(this.state.array);
-        console.log(testSort, sortedArray)
-
-        console.log(arraysAreEqual(testSort, sortedArray));
-    }
-
-    testSortingAlgos() {
-        for (let i = 0; i < 100; i++) {
-            const array = [];
-            const length = randomInteger(1, 1000);
-            for (let i = 0; i < length; i++) {
-                array.push(randomInteger(-1000, 1000));
-            } 
-            const testSort = this.state.array.slice().sort((a, b) => a - b);
-            const sortedArray = mergeSort(this.state.array);
-            console.log(arraysAreEqual(testSort, sortedArray))
+        // const testSort = this.state.array.slice().sort((a, b) => a - b);
+        const animations = sortingAlgorithms.mergeSort(this.state.array);
+        console.log(animations[0])
+        const newAnimations = [];
+        for (const animation of animations) {
+            console.log(animation.comparison)
+            newAnimations.push(animation.comparison);
+            newAnimations.push(animation.comparison);
+            newAnimations.push(animation.swap);
+            console.log(newAnimations)
         }
+        for (let i = 0; i < newAnimations.length; i++) {
+            const arrayBars = document.getElementsByClassName('array-bar');
+          
+            const isColorChange = i % 3 !== 2;
+            if (isColorChange) {
+                const [barOneIdx, barTwoIdx] = newAnimations[i];
+                const barOneStyle = arrayBars[barOneIdx].style;
+                const barTwoStyle = arrayBars[barTwoIdx].style;
+                const color = i % 3 === 0 ? 'red' : 'slategray';
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = color;
+                    barTwoStyle.backgroundColor = color;
+                }, i * 5);
+            } else {
+                setTimeout(() => {
+                    const [barOneIdx, newHeight] = newAnimations[i]
+                    const barOneStyle = arrayBars[barOneIdx].style;
+                    barOneStyle.height = `${newHeight}px`;
+                    // barOneStyle.height = barTwoStyle.height;
+                    // barTwoStyle.height = tempHeight;
+                }, i * 5)
+            }
+        }
+
+
+        // for (let i = 0; i < animations.length; i++) {
+        //     const { comparison, swap } = animations[i];
+        //     setTimeout(()=> {
+        //         const arrayBars = document.getElementsByClassName('array-bar');
+        //         arrayBars[comparison[1]].style.backgroundColor = red;
+        //         arrayBars[comparison[0]].style.backgroundColor = red;
+        //         setTimeout(() => {
+        //             arrayBars[comparison[1]].style.backgroundColor = slategray;
+        //             arrayBars[comparison[0]].style.backgroundColor = slategray;
+        //         }, (i + 1) * 10);
+        //     }, i * 10)
+        // }
+      
+        // console.log(arraysAreEqual(testSort, sortedArray));
     }
+
+    // testSortingAlgos() {
+    //     for (let i = 0; i < 100; i++) {
+    //         const array = [];
+    //         const length = randomInteger(1, 1000);
+    //         for (let i = 0; i < length; i++) {
+    //             array.push(randomInteger(-1000, 1000));
+    //         } 
+    //         const testSort = this.state.array.slice().sort((a, b) => a - b);
+    //         const sortedArray = mergeSort(this.state.array);
+    //         console.log(arraysAreEqual(testSort, sortedArray))
+    //     }
+    // }
 
     render() {
         const {array} = this.state;
--- src/algorithms/sortingAlgorithms.js
+++ src/algorithms/sortingAlgorithms.js
@@ -1,12 +1,14 @@
 
-function mergeSort(array) {
+export const mergeSort = array => {
+    const animations = []
     if (array.length <= 1) return array;
     const auxiliaryArray = array.slice();
-    mergeSortHelper(array, 0, array.length - 1, auxiliaryArray)
-    return array;
+    mergeSortHelper(array, 0, array.length - 1, auxiliaryArray, animations)
+    return animations;
 } 
 
 function mergeSortHelper(mainArray, startIdx, endIdx, auxiliaryArray, animations) {
+
     if (startIdx === endIdx) return;
     const middleIdx = Math.floor((startIdx + endIdx) / 2)
     mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, animations);
@@ -19,18 +21,33 @@ function doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animati
     let i = startIdx;
     let j = middleIdx + 1;
     while (i <= middleIdx && j <= endIdx) {
+        const animation = {};
+        animation.comparison = [i, j]
         if (auxiliaryArray[i] <= auxiliaryArray[j]) {
+            animation.swap = [k, auxiliaryArray[i]]
             mainArray[k++] = auxiliaryArray[i++]
         } else {
+            animation.swap = [k, auxiliaryArray[j]]
             mainArray[k++] = auxiliaryArray[j++]
         }
+        animations.push(animation)
     }
     while (i <= middleIdx) {
+        animations.push({
+            comparison: [i, i],
+            swap: [k, auxiliaryArray[i]]
+        })
+        
         mainArray[k++] = auxiliaryArray[i++]
     }
     while (j <= endIdx) {
+        animations.push({
+            comparison: [j, j],
+            swap: [k, auxiliaryArray[j]]
+        })
         mainArray[k++] = auxiliaryArray[j++]
     }
+    console.log(animations)
 }
 
 // if (array.length <= 1) return array;
