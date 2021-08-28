589a12a9246c3a6173f27e618ae1f1397fe5e68c
Laura Aupert <aupert.laura@gmail.com>
2021-08-28 15:13:10 -0400

    Created the merge sort file and a test for the sorted arrays

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './SortingVisualizer.css'
+import mergeSort from '../algorithms/mergeSort'
 
 export default class SortingVisualizer extends React.Component {
     constructor(props) {
@@ -11,15 +12,36 @@ export default class SortingVisualizer extends React.Component {
     }   
 
     componentDidMount() {
-        this.resetArray();
+        this.generateArray();
     }
 
-    resetArray() {
+    generateArray() {
         const array = [];
         for (let i = 0; i < 100; i++) {
             array.push(randomInteger(5, 1000));
         }
         this.setState({array});
+    }
+
+    mergeSort() {
+        const testSort = this.state.array.slice().sort((a, b) => a - b);
+        const sortedArray = mergeSort(this.state.array);
+        console.log(testSort, sortedArray)
+
+        console.log(arraysAreEqual(testSort, sortedArray));
+    }
+
+    testSortingAlgos() {
+        for (let i = 0; i < 100; i++) {
+            const array = [];
+            const length = randomInteger(1, 1000);
+            for (let i = 0; i < length; i++) {
+                array.push(randomInteger(-1000, 1000));
+            } 
+            const testSort = this.state.array.slice().sort((a, b) => a - b);
+            const sortedArray = mergeSort(this.state.array);
+            console.log(arraysAreEqual(testSort, sortedArray))
+        }
     }
 
     render() {
@@ -34,6 +56,11 @@ export default class SortingVisualizer extends React.Component {
                         style={{height: `${value / 12}vh`}}>
                     </div>
                 ))}
+                <div>
+                    <button onClick={() => this.generateArray()}>New Array</button>
+                    <button onClick={() => this.mergeSort()}>Merge Sort</button>
+                    <button onClick={() => this.testSortingAlgos()}>Test Algorithms</button>
+                </div>
             </div>
         )
     }
@@ -45,5 +72,12 @@ function randomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+function arraysAreEqual(testArray, algoArray) {
+    if (testArray.length !== algoArray.length) return false;
+    for (let i = 0; i < testArray.length; i++) {
+        if (testArray[i] !== algoArray[i]) return false;
+    }
+    return true;
+}
 
 
---
+++ src/algorithms/mergeSort.js
@@ -0,0 +1,63 @@
+
+function mergeSort(array) {
+    if (array.length <= 1) return array;
+    const auxiliaryArray = array.slice();
+    mergeSortHelper(array, 0, array.length - 1, auxiliaryArray)
+    return array;
+} 
+
+function mergeSortHelper(mainArray, startIdx, endIdx, auxiliaryArray, animations) {
+    if (startIdx === endIdx) return;
+    const middleIdx = Math.floor((startIdx + endIdx) / 2)
+    mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, animations);
+    mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray, animations);
+    doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations)
+}
+
+function doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations) {
+    let k = startIdx;
+    let i = startIdx;
+    let j = middleIdx + 1;
+    while (i <= middleIdx && j <= endIdx) {
+        if (auxiliaryArray[i] <= auxiliaryArray[j]) {
+            mainArray[k++] = auxiliaryArray[i++]
+        } else {
+            mainArray[k++] = auxiliaryArray[j++]
+        }
+    }
+    while (i <= middleIdx) {
+        mainArray[k++] = auxiliaryArray[i++]
+    }
+    while (j <= endIdx) {
+        mainArray[k++] = auxiliaryArray[j++]
+    }
+}
+
+// if (array.length <= 1) return array;
+//     const middleIdx = Math.floor(array.length / 2);
+//     const leftHalf = array.slice(0, middleIdx);
+//     const rightHalf = array.slice(middleIdx);
+//     return mergeSortedArrays(mergeSort(leftHalf), mergeSort(rightHalf));
+// }
+
+// export default mergeSort;
+
+// function mergeSortedArrays(leftHalf, rightHalf) {
+//     const sortedArray = new Array(leftHalf.length + rightHalf.length);
+//     let k = 0;
+//     let i = 0;
+//     let j = 0;
+//     while (i < leftHalf.length && j < rightHalf.length) {
+//         if (leftHalf[i] <= rightHalf[j]) {
+//             sortedArray[k++] = leftHalf[i++]
+//         } else {
+//             sortedArray[k++] = rightHalf[j++];
+//         }
+//     }
+//     while (i < leftHalf.length) {
+//         sortedArray[k++] = leftHalf[i++]
+//     }
+//     while (j < rightHalf.length) {
+//         sortedArray[k++] = rightHalf[j++]
+//     }
+//     return sortedArray;
